a3fb77b25cd1dac0b31a327fc82d2529a8bd03c7
dbaldin <dib@gmx.net>
2015-08-12 20:25:05 +0200

    - newlib added for userspace libc and libm support

---
+++ library/libc/include/newlib.h
@@ -0,0 +1,5 @@
+/* dummy file for external tools to use.  Real file is created by
+   newlib configuration. */
+
+//#define STRING_ONLY 1
+//#define INTEGER_ONLY 1
---
+++ library/libc/include/sys/dirent.h
@@ -0,0 +1,8 @@
+/* <dirent.h> includes <sys/dirent.h>, which is this file.  On a
+   system which supports <dirent.h>, this file is overridden by
+   dirent.h in the libc/sys/.../sys directory.  On a system which does
+   not support <dirent.h>, we will get this file which uses #error to force
+   an error.  */
+
+#include_next <sys/dirent.h>
+
